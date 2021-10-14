1bf52c5c5d36a8456d51049b877fe6244595127b
“Rajul” <rajulkoshta10@gmail.com>
2021-10-14 21:12:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "./note";
+
+function AddDesc(desc){
+  return (
+      <Note 
+        title = {desc.title}
+        content = {desc.content}
+      />
+  );
+}
 
 function App(){
     return (
         <div>
           <Header />
-          <Note />
+         {notes.map(AddDesc)}
           <Footer />
         </div>
     );
