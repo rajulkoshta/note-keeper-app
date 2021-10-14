1bc927c043f0271912206cf3ebfe8cd86c1581ca
“Rajul” <rajulkoshta10@gmail.com>
2021-10-14 21:25:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,18 @@ import Note from "./Note";
 import Footer from "./Footer";
 import notes from "./note";
 
-function AddDesc(desc){
-  return (
-      <Note 
-        key = {desc.key}
-        title = {desc.title}
-        content = {desc.content}
-      />
-  );
-}
 
 function App(){
     return (
         <div>
           <Header />
-         {notes.map(AddDesc)}
+         {notes.map(desc => 
+      <Note 
+        key = {desc.key}
+        title = {desc.title}
+        content = {desc.content}
+      />
+)}
           <Footer />
         </div>
     );
