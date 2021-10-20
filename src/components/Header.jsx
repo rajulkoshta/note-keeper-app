e3c19d311c4c95361519e7465d556deac96a2040
“Rajul” <rajulkoshta10@gmail.com>
2021-10-20 21:30:01 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
-function Header(){
-    return (
-        <header>
-            <h1>Keeper App</h1>
-        </header>
-    );
+import OfflineBoltIcon from '@material-ui/icons/OfflineBolt';
+function Header() {
+  return (
+    <header>
+      <h1>
+         <OfflineBoltIcon /> Keeper, keeps you update</h1>
+    </header>
+  );
 }
 
 export default Header;
